fa6b4041d7eaa977b75e914e012eb9237fb39cb4
Leonard <oseghaleleonard39@gmail.com>
2025-03-15 10:45:05 -0700

    Edited the main codebase

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,45 @@
 fn main() {
 
     
-    
-    name();
-    mat_no();
-    favorite_emoji();
-    let value = difference();
+    // Printing my name
+    name("Leonard");
+
+    // Printing my Mat No
+    mat_no("ENG2204523");
+
+    // Printing my favorite Emoji
+    favorite_emoji('🌹');
+
+     // The difference of two entered numbers
+    let value = difference(64, 43);
+
+    // Thge difference expressed as fraction/decimal
     formatted(value);
 }
 
-fn name(){
-    // Printing my name
-    let name: String = String::from("Leonard");
-
+fn name(name: &str){  
     println!("My name is: {name}");
 }
 
-fn mat_no() {
-    // Printing my Mat No
-    let mat_no: String = String::from("ENG2204523");
-
-    println!("My Matriculation number is: {mat_no}");
+fn mat_no(num: &str) {
+    println!("My Matriculation number is: {num}");
 }
 
-fn favorite_emoji() {
-    // Printing my favorite Emoji
-    let favorite_emoji = String::from("😎");
-
-    println!("My favorite emoji is: {favorite_emoji}");
+fn favorite_emoji(favorite: char) {
+    println!("My favorite emoji is: {favorite}");
 }
 
-fn difference() -> i32 {
-    // The difference of two entered numbers
-    let num1: i32 = 78;
-    let num2: i32 = 45;
+fn difference(num1: i32, num2: i32) -> i32 {
+   
     let difference = num1 - num2;
-
-    println!("The difference between the two numbers entered is: {difference}");
-
+    println!("The difference between the two numbers is: {difference}");
     difference as i32
 }
 
 fn formatted(result: i32) {
-    //making the result of the dfference into a fraction/dcimal
-    let integer: i32 = result;
-    let float: f64 = integer as f64;
-    let formatted = format!("{:.2}", float);
+    //making the result of the dfference into a fraction/decimal
+    let integer = result as f64;
+    let formatted = format!("{:.2}", integer);
 
     println!("Formatted float: {formatted}");
 }
--- src/side.rs
+++ src/side.rs
@@ -1,48 +1,31 @@
+
+
+
 fn main() {
-
-    name();
-    mat_no();
-    favorite_emoji();
-    difference();
-    formatted();
-}
-
-fn name(){
     // Printing my name
     let name: String = String::from("Leonard");
 
-    println!("{name}");
-}
-
-fn mat_no() {
     // Printing my Mat No
     let mat_no: String = String::from("ENG2204594");
 
-    println!("{mat_no}");
-}
-
-fn favorite_emoji() {
     // Printing my favorite Emoji
     let favorite_emoji = String::from("😎");
 
-    println!("{favorite_emoji}");
-}
 
-fn difference() {
     // The difference of two entered numbers
     let num1 = 98;
     let num2 = 45;
+
     let difference = num1 - num2;
 
-    println!("{difference}");
-}
-
-fn formatted() {
     //making the result of the difference into a fraction/dcimal
     let integer: i32 = difference;
     let float: f64 = integer as f64;
     let formatted = format!("{:.2}", float);
 
+    println!("{name}");
+    println!("{mat_no}");
+    println!("{favorite_emoji}");
+    println!("{difference}");
     println!("Formatted float: {formatted}");
 }
-
